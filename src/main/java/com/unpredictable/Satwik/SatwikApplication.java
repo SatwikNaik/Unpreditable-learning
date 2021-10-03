3702d88af84d5585ad5907f5ed1163e0a7499a69
SatwikNaik <naiksatwik99@gmail.com>
2021-10-03 22:55:15 +0530

    Database Commit

---
+++ src/main/java/com/unpredictable/Satwik/SatwikApplication.java
@@ -0,0 +1,17 @@
+package com.unpredictable.Satwik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+
+public class SatwikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatwikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unpredictable/Satwik/SatwikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unpredictable.Satwik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatwikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
